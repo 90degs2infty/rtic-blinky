067638ee0e1643d173f4d035cfdf75f1e9f9b399
90degs2infty <90degs2infty@posteo.org>
2023-07-18 14:56:17 +0200

    Add module-level documentation to crate::timer::state

--- src/timer/state.rs
+++ src/timer/state.rs
@@ -1,3 +1,7 @@
+//! Hal-level interface to a timer's state.
+//!
+//! See [Nordic's docs](https://infocenter.nordicsemi.com/topic/ps_nrf52840/timer.html?cp=5_0_0_5_29) for details.
+
 /// Type indicating a running timer.
 pub struct Started;
 
