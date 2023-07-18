ba23b8e3038fecc2a674cc3fc07f872d77bcbe19
90degs2infty <90degs2infty@posteo.org>
2023-07-18 14:55:21 +0200

    Add module-level documentation to crate::timer::mode

--- src/timer/mode.rs
+++ src/timer/mode.rs
@@ -1,3 +1,7 @@
+//! Hal-level interface to a timer's timer mode.
+//!
+//! See [Nordic's docs](https://infocenter.nordicsemi.com/topic/ps_nrf52840/timer.html?cp=5_0_0_5_29) for details.
+
 use crate::timer::prescaler::Prescaler;
 
 use core::marker::PhantomData;
