5b2617800d3d2846ae67b5c4bdad0b9c242f48bb
90degs2infty <90degs2infty@posteo.org>
2023-07-18 14:31:36 +0200

    Add module level documentation to interrupt state

--- src/timer/interrupts.rs
+++ src/timer/interrupts.rs
@@ -1,3 +1,7 @@
+//! Hal-level interface to a timer's interrupts.
+//!
+//! See [Nordic's docs](https://infocenter.nordicsemi.com/topic/ps_nrf52840/timer.html?cp=5_0_0_5_29) for details.
+
 /// Type indicating a timer triggering interrupts.
 pub struct Enabled;
 
